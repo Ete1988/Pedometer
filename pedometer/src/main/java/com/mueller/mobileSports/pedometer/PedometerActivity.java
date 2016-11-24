0fadbe6f80e4c39751842b678c4e7fa62bcc0466
Ete <samura1988@freenet.de>
2016-11-24 15:58:15 +0100

    Optimizes shared prefs

--- pedometer/src/main/java/com/mueller/mobileSports/pedometer/PedometerActivity.java
+++ pedometer/src/main/java/com/mueller/mobileSports/pedometer/PedometerActivity.java
@@ -34,7 +34,6 @@ public class PedometerActivity extends AppCompatActivity implements SensorEventL
     private TextView date;
     private int stepsOverWeek;
     private int stepsOverDay;
-
     private sharedValues values;
 
     @Override
@@ -65,92 +64,6 @@ public class PedometerActivity extends AppCompatActivity implements SensorEventL
         values.saveInt("weekCount", stepsOverWeek);
     }
 
-    /*
-
-
-        private void updateDayCount() {
-            try {
-                boolean check;
-                check = checkIfNewDay();
-                isNewDay(check);
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-        }
-
-        private void isNewDay(boolean checkDay) {
-            if (checkDay) {
-                stepsOverDay = 0;
-            } else {
-                stepsOverDay = myData.getInt("dayCount", 0);
-            }
-        }
-
-        private boolean checkIfNewDay() throws ParseException {
-
-            TextView date = (TextView) findViewById(R.id.date);
-            Date todayDate = new Date();
-            SimpleDateFormat currDate = new SimpleDateFormat("EE dd MMM yyyy");
-            String currentDate = currDate.format(todayDate);
-            date.setText(currentDate);
-
-            if (!(myData.contains("checkDate"))) {
-                editor.putString("checkDate", currentDate);
-                return true;
-            } else {
-
-                Date oldDate = currDate.parse((myData.getString("checkDate", null)));
-                Date now = currDate.parse(currentDate);
-
-                if (oldDate.before(now)) {
-                    editor.putInt("dayCount", 0);
-                    editor.putString("checkDate", currentDate);
-                    return true;
-                } else if (oldDate.equals(now)) {
-                    editor.putInt("dayCount", stepsOverDay);
-                    return false;
-                } else {
-                    //should never be reached
-                    //TODO test this out
-                    return true;
-                }
-            }
-        }
-
-        //TODO test this.
-        private void updateWeekCount() {
-
-            boolean checkWeek = checkIfNewWeek();
-            if (checkWeek) {
-                stepsOverWeek = 0;
-            } else {
-                stepsOverWeek = myData.getInt("weekCount", 0);
-            }
-        }
-
-        private boolean checkIfNewWeek() {
-
-            Calendar c = Calendar.getInstance();
-            if (!myData.contains("weekOfYear")) {
-                editor.putInt("weekOfYear", c.get(Calendar.WEEK_OF_YEAR));
-                return true;
-            } else {
-
-                if ((c.get(Calendar.WEEK_OF_YEAR)) > (myData.getInt("weekOfYear", 0))) {
-                    editor.putInt("weekCount", 0);
-                    editor.putInt("weekOfYear", c.get(Calendar.WEEK_OF_YEAR));
-                    return true;
-                } else if ((c.get(Calendar.WEEK_OF_YEAR)) == (myData.getInt("weekOfYear", 0))) {
-                    editor.putInt("weekCount", stepsOverWeek);
-                    return false;
-                } else {
-                    //should never be reached
-                    //TODO test this out
-                    return false;
-                }
-            }
-        }
-    */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -212,7 +125,7 @@ public class PedometerActivity extends AppCompatActivity implements SensorEventL
     protected void onDestroy() {
         super.onDestroy();
         updateData();
-        //editor.apply();
+
 
     }
 
@@ -222,7 +135,7 @@ public class PedometerActivity extends AppCompatActivity implements SensorEventL
         updateData();
         // if you unregister the last listener, the hardware will stop detecting step events
         //sensorManager.unregisterListener(this);
-        //editor.apply();
+
     }
 
     @Override
