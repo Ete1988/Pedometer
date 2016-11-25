8595aa9db676bf7873efd61bdcc47bda28752713
Ete <samura1988@freenet.de>
2016-11-25 18:40:24 +0100

    splash fine maybe

--- pedometer/src/main/java/com/mueller/mobileSports/general/SplashActivity.java
+++ pedometer/src/main/java/com/mueller/mobileSports/general/SplashActivity.java
@@ -1,16 +1,8 @@
 package com.mueller.mobileSports.general;
 
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.CountDownTimer;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-
-import com.mueller.mobileSports.pedometer.MainActivity.R;
 
 
 public class SplashActivity extends AppCompatActivity {
@@ -21,37 +13,6 @@ public class SplashActivity extends AppCompatActivity {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         finish();
-
-   /*     requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        View decorView = getWindow().getDecorView();
-        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-        decorView.setSystemUiVisibility(uiOptions);
-
-        setContentView(R.layout.activity_splash);
-
-
-        new CountDownTimer(3000,3000){
-            @Override
-            public void onTick(long millisUntilFinished){}
-
-            @Override
-            public void onFinish(){
-                //set the new Content of your activity
-                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        }.start();
-
-        */
     }
-/*
-    public void onBackPressed(){
-        super.onBackPressed();
-        this.finish();
-    }
-
-*/
 
 }
